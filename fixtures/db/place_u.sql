d5214abd2c7e7f25b02cd1123be2da92c0aff911
Björn Hempel <bjoern@hempel.li>
2022-05-22 00:28:05 +0200

    #92; Calendar: Evaluate specified location; Add fixtures and .gitignore; Update table-dumper

---
+++ fixtures/db/place_u.sql
@@ -0,0 +1,46 @@
+-- MariaDB dump 10.19  Distrib 10.7.1-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: php-calendar-api
+-- ------------------------------------------------------
+-- Server version	10.7.3-MariaDB-1:10.7.3+maria~focal-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `place_u`
+--
+
+LOCK TABLES `place_u` WRITE;
+/*!40000 ALTER TABLE `place_u` DISABLE KEYS */;
+INSERT INTO `place_u` VALUES
+(200417,7283696,'HVDC Baltic Cable - Cathode','HVDC Baltic Cable - Cathode','','\0\0\0\0\0\0\0Ï ¡K@¹ˆïÄ¬G&@','U','RDGU','DE','',0,0,-9999,'Europe/Berlin','2010-03-24','2022-05-09 17:14:56','2022-05-09 17:14:56','12',NULL,NULL,NULL),
+(334406,8081323,'Isla del Descubridor','Isla del Descubridor','','\0\0\0\0\0\0\0ñcÌ]K@C@æØG§®À¿','U','SMU','ES','',0,0,0,'Europe/Madrid','2011-12-12','2022-05-10 01:13:10','2022-05-10 01:13:10','60','A','03031',NULL),
+(497149,8739859,'Cafe Grappa','Cafe Grappa','','\0\0\0\0\0\0\0µ7øÂdJ@™dä,ô4@','U','CRSU','PL','',0,0,109,'Europe/Warsaw','2014-03-16','2022-05-10 18:46:35','2022-05-10 18:46:35','78','1465','146501',NULL),
+(637478,10345399,'Gisse 100 m ude','Gisse 100 m ude','','\0\0\0\0\0\0\0ïæ©¹½K@\\¬¨Á¤,@','U','BNKU','SE','',0,0,-9999,'Europe/Stockholm','2015-11-02','2022-05-11 20:06:41','2022-05-11 20:06:41','27','1291','129101','1167'),
+(645516,2614545,'RÃ¸nnen','Ronnen','','\0\0\0\0\0\0\0>\"¦DÂK@¼\"øßJ&&@','U','SHLU','DK','',0,0,-9999,'Europe/Copenhagen','2017-09-12','2022-05-12 15:07:12','2022-05-12 15:07:12','20','326',NULL,NULL),
+(649585,2619259,'Jordsand','Jordsand','Jordsand','\0\0\0\0\0\0\0n‹2d‚K@}‘Ð–s!@','U','BNKU','DK','',0,0,-9999,'Europe/Copenhagen','2018-05-27','2022-05-12 15:40:10','2022-05-12 15:40:10','21','550',NULL,NULL),
+(656177,10345377,'Timannevej 100 m ude','Timannevej 100 m ude','','\0\0\0\0\0\0\0#óÈL@\'ˆº@z(@','U','BKSU','DK','',0,0,-9999,'Europe/Copenhagen','2015-06-22','2022-05-12 16:31:27','2022-05-12 16:31:27','17',NULL,NULL,NULL),
+(656178,10345378,'RÃ¥geleje 100 m ude','Rageleje 100 m ude','','\0\0\0\0\0\0\0\"\ZÝAìL@r§t°þO(@','U','BKSU','DK','',0,0,-9999,'Europe/Copenhagen','2015-06-22','2022-05-12 16:31:27','2022-05-12 16:31:27','17','270',NULL,NULL),
+(656179,10345379,'Tisvilleleje 100 m ude','Tisvilleleje 100 m ude','','\0\0\0\0\0\0\0¶J°8L@iÆ¢é(@','U','BNKU','DK','',0,0,-9999,'Europe/Copenhagen','2015-06-22','2022-05-12 16:31:28','2022-05-12 16:31:28','17',NULL,NULL,NULL),
+(656180,10345400,'RÃ¸dvig 100 m ude','Rodvig 100 m ude','','\0\0\0\0\0\0\0j\'÷; K@–	¿ÔÏË(@','U','BNKU','DK','',0,0,-9999,'Europe/Copenhagen','2015-06-22','2022-05-12 16:31:28','2022-05-12 16:31:28','20','336',NULL,NULL),
+(656181,10345413,'Busende have 100 m ude','Busende have 100 m ude','','\0\0\0\0\0\0\0•·#œxK@Íé²˜Ø)@','U','BNKU','DK','',0,0,-9999,'Europe/Copenhagen','2017-06-22','2022-05-12 16:31:29','2022-05-12 16:31:29','20','390',NULL,NULL);
+/*!40000 ALTER TABLE `place_u` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-22  0:00:50
