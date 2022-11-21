8ee367b65b6bb8c8add2a1575f3828933f62f039
Björn Hempel <bjoern@hempel.li>
2022-11-21 02:47:35 +0100

    #106; Symfony, API Platform, PHP: Update to Symfony 6.1, API Platform 3.0.3 and PHP 8.1.12; Add RefreshToken entity

---
+++ src/Entity/RefreshToken.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the bjoern-hempel/php-calendar-api project.
+ *
+ * (c) Björn Hempel <https://www.hempel.li/>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+
+/**
+ * Entity class RefreshToken
+ *
+ * @author Björn Hempel <bjoern@hempel.li>
+ * @version 0.1.0 (2022-11-21)
+ * @since 0.1.0 (2022-11-21) First version.
+ * @package App\Entity
+ */
+#[ORM\Entity]
+#[ORM\Table("refresh_tokens")]
+class RefreshToken extends BaseRefreshToken
+{
+}
